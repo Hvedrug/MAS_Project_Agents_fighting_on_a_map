a6679cd24dad5aed3a4b781611d3cb32cd13aeef
Hvedrug <63800678+Hvedrug@users.noreply.github.com>
2022-02-10 15:23:04 +0100

    Add files via upload

---
+++ src/listKillerVsKiller/importEnv.py
@@ -0,0 +1,5 @@
+import src.listKillerVsKiller.Environment as Environment
+import src.listKillerVsKiller.Policy as Policy
+import src.listKillerVsKiller.QLearning as QLearning
+
+filename = "data/listKillerVsKiller.txt"
